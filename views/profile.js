400fda483082b201d412adb9342a6cd9724cef76
Jon Ramer <jramer87@gmail.com>
2017-01-08 19:22:52 -0600

    added click events for profile forms

--- views/profile.js
+++ views/profile.js
@@ -3,22 +3,61 @@
 $(document)
     .ready(function() {
         console.log("it's working");
-        $('#addHack')
+        $('#newHack')
             .on('click', function(event) {
-                console.log('clicked');
                 event.preventDefault();
-                let formData = {};
-                $('.addHack')
-                    .each(function() {
-                        if (this.id !== 'addHack') {
-                            console.log($(this));
-                            formData[this.id] = $(this)
-                                .val();
-                        }
-                        console.log(formData);
-                        //     formData['additional'] = $('#additional')
-                        //         .value;
-                    });
+                console.log('you clicked new hack');
+                let newitionalHackCategoryId = $('#newHackCategoryId')
+                    .val();
+                let newHackName = $('#newHackName')
+                    .val();
+                let newHackDescription = $('#newHackDescription')
+                    .val();
+                let newHackUrl = $('#newHackUrl')
+                    .val();
+                let newHackUserId = $('#newHackUserId')
+                    .val();
+                console.log(newHackCategoryId, newHackName, newHackDescription, newHackUrl, newHackUserId);
+                // $.post({
+                //     data: JSON.stringify(formData),
+                //     url: '/lifehacks',
+                //     dataType: 'json',
+                //     contentType: 'application/json; charset=UTF-8'
+                // });
+            });
+        $('#updateHackButton')
+            .on('click', function(event) {
+                event.preventDefault();
+                console.log('you clicked update hack');
+                let updateHackCategoryId = $('#updateHackCategory')
+                    .val();
+                let updateHackPostId = $('#updateHackPostId')
+                    .val();
+                let updateHackName = $('#updateHackName')
+                    .val();
+                let updateHackDescription = $('#updateHackDescription')
+                    .val();
+                let updateHackUrl = $('#updateHackUrl')
+                    .val();
+                let updateHackUserId = $('#updateHackUserId')
+                    .val();
+                console.log(updateHackCategoryId, updateHackPostId, updateHackName, updateHackDescription, updateHackUrl, updateHackUserId);
+                $.post({
+                    data: JSON.stringify(formData),
+                    url: '/lifehacks',
+                    dataType: 'json',
+                    contentType: 'application/json; charset=UTF-8'
+                });
+            });
+        $('#deleteHackButton')
+            .on('click', function(event) {
+                event.preventDefault();
+                console.log('you clicked delete hack');
+                let deleteHackCategoryId = $('#deleteHackCategory')
+                    .val();
+                let deleteHackPostId = $('#deleteHackPostId')
+                    .val();
+                console.log(deleteHackPostId, deleteHackPostId);
                 $.post({
                     data: JSON.stringify(formData),
                     url: '/lifehacks',
