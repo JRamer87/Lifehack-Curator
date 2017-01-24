6cdc73516c229c7de454066dc9f4266233ea5339
Jon Ramer <jramer87@gmail.com>
2017-01-24 15:40:29 -0600

    updated knex config settings

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,51 @@
-'use strict';
+"use strict";
 
 module.exports = {
+
     development: {
         client: 'pg',
-        connection: 'postgres://localhost/lifehacks'
+        connection: {
+            database: 'glimpse-dev',
+            host: 'localhost'
+        }
     },
 
     test: {
         client: 'pg',
-        connection: 'postgres://localhost/lifehacks'
+        connection: {
+            database: 'glimpse-test',
+            host: 'localhost',
+        }
     },
+
+    production: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL,
+    }
+
+    // production: {
+    //   client: 'postgresql',
+    //   connection: {
+    //     database: 'my_db',
+    //     user:     'username',
+    //     password: 'password'
+    //   },
+    //   pool: {
+    //     min: 2,
+    //     max: 10
+    //   },
+    //   migrations: {
+    //     tableName: 'knex_migrations'
+    //   }
+    // }
+
+};
+min: 2,
+    //     max: 10
+    //   },
+    //   migrations: {
+    //     tableName: 'knex_migrations'
+    //   }
+    // }
 
 };
